a8ac7b25a096c9648510b3185d0649c0da361be7
duttong <geoff.dutton@noaa.gov>
2020-12-17 13:50:18 -0700

    ssv_add to 9

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ aeris_logfile = 'aeris-log.txt'
 aeris_datafile = f'aeris-{datetime.utcnow().strftime("%Y%m%d-%H%M%S")}.csv'
 
 # create a basic valve sequence
-repeat = 2      # number of times to repeat valve sequence
-dur = 60        # seconds on each port
+repeat = 3      # number of times to repeat valve sequence
+dur = 300        # seconds on each port
 valve_seq = [1, 2, 3, 4]
 
 seq = [(pos, n, dur) for n in range(repeat) for pos in valve_seq]
