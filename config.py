190c972eb2849d519e62f230a5ea29e03433785c
duttong <geoff.dutton@noaa.gov>
2020-12-17 13:50:30 -0700

    ssv add to 9

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 from datetime import datetime
 
-ssv_add = 1
-ssv_port = '/dev/cu.usbserial-14640'
-aeris_port = '/dev/cu.usbserial-1420'
+ssv_add = 9
+# ssv_port = '/dev/cu.usbserial-14640'
+# aeris_port = '/dev/cu.usbserial-1420'
+ssv_port = '/dev/ttyUSB0'
+aeris_port = '/dev/ttyS0'
 
 aeris_logfile = 'aeris-log.txt'
 aeris_datafile = f'aeris-{datetime.utcnow().strftime("%Y%m%d-%H%M%S")}.csv'
 
 # create a basic valve sequence
-repeat = 3      # number of times to repeat valve sequence
-dur = 300        # seconds on each port
-valve_seq = [1, 2, 3, 4]
+repeat = 2      # number of times to repeat valve sequence
+dur = 30        # seconds on each port
+valve_seq = [2, 4]
 
 seq = [(pos, n, dur) for n in range(repeat) for pos in valve_seq]
