05ab19d45f8e17354e1cad985df0c711243cd163
duttong <geoff.dutton@noaa.gov>
2020-12-18 14:36:18 -0700

    update

--- stats.py
+++ stats.py
@@ -1,7 +1,13 @@
 #! /usr/bin/env python
 
 import pandas as pd
+import numpy as np
 import argparse
+
+
+def pct(x):
+    return np.std(x)/np.mean(x)*100
+
 
 opt = argparse.ArgumentParser(description='Quick stats on Aeris data file.')
 opt.add_argument('csvfile', help='Aeris .csv data file.')
@@ -10,4 +16,4 @@ options = opt.parse_args()
 file = options.csvfile
 df = pd.read_csv(file, infer_datetime_format=True, parse_dates=True, index_col='datetime')
 df2 = df.groupby(['seq_count', 'ssv'], as_index=False).apply(lambda x: x.iloc[x.ssv.size//2:]).reset_index(level=0, drop=True)
-print(df2.groupby(['seq_count', 'ssv'])['n2o', 'co'].agg(['mean', 'std']))
+print(df2.groupby(['seq_count', 'ssv'])['n2o', 'co'].agg(['mean', 'std', 'pct']))
