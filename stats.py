2a8be896a328171d734ce4fa8a5b75132702488d
duttong <geoff.dutton@noaa.gov>
2020-12-23 11:55:15 -0700

    added percent calculation

--- stats.py
+++ stats.py
@@ -15,5 +15,5 @@ options = opt.parse_args()
 
 file = options.csvfile
 df = pd.read_csv(file, infer_datetime_format=True, parse_dates=True, index_col='datetime')
-df2 = df.groupby(['seq_count', 'ssv'], as_index=False).apply(lambda x: x.iloc[x.ssv.size//2:]).reset_index(level=0, drop=True)
-print(df2.groupby(['seq_count', 'ssv'])['n2o', 'co'].agg(['mean', 'std', 'pct']))
+df2 = df.groupby(['seq_count', 'ssv'], as_index=False).apply(lambda x: x.iloc[x.ssv.size//3:]).reset_index(level=0, drop=True)
+print(df2.groupby(['seq_count', 'ssv'])['n2o', 'co'].agg(['mean', 'std', pct, 'count']))
